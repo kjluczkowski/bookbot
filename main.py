d412b55ba0842b68fc92a12d1ff2141ede6b1513
kluczkow <kyleluczkowski@gmail.com>
2025-09-01 12:27:02 -0700

    Next update

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+from stats import count_words, count_chars
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     
     word_count = count_words(text)
 
+    character_count = count_chars(text)
+
     print(f"{word_count} words found in the document")
+
+    print(character_count)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 
 main()
